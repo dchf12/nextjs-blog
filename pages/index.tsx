46869a76833c33dbf4d6f6b1e57a3a237efe326f
dchf <dchf.jp@gmail.com>
2023-05-14 00:00:48 +0900

    layout修正

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,45 +1,21 @@
-import Link from 'next/link';
 import Head from 'next/head';
+import Layout, { siteTitle } from '../components/layout';
 import styles from '../styles/Home.module.css';
 
 export default function Home(): JSX.Element {
     return (
-        <div className={styles.container}>
+        <Layout home>
             <Head>
-                <title>Create Next App</title>
-                <link rel="icon" href="/favicon.ico" />
+                <title>{siteTitle}</title>
             </Head>
-
-            <main>
-                <h1 className={styles.title}>
-                    Read <Link href="/posts/first-post">this page!</Link>
-                </h1>
-            </main>
-
-            <style jsx>{`
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-      `}</style>
-
-            <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-        </div>
-    )
+            <section className={styles.headingMd}>
+                <p>Hello, I'm Daichi</p>
+                <p>
+                    (This is a sample website - you’ll be building a site like this on{' '}
+                    <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+                </p>
+            </section>
+        </Layout>
+    );
 }
+
