9cafcecc20acda1a0b9155ccc0e94b86a7c2e244
dchf <dchf.jp@gmail.com>
2023-05-16 21:32:12 +0900

    内部ファイルを読み込みSSGでプリレンダリング

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import styles from '../styles/Home.module.css';
+import utilStyles from '../styles/utils.module.css';
+import { getSortedPostsData } from '../lib/posts';
 
-export default function Home(): JSX.Element {
+export default function Home({ allPostsData }): JSX.Element {
     return (
         <Layout home>
             <Head>
@@ -15,7 +17,29 @@ export default function Home(): JSX.Element {
                     <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
                 </p>
             </section>
+            <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+                <h2 className={utilStyles.headingLg}>Blog</h2>
+                <ul className={utilStyles.list}>
+                    {allPostsData.map(({ id, date, title }) => (
+                        <li className={utilStyles.listItem} key={id}>
+                            {title}
+                            <br />
+                            {id}
+                            <br />
+                            {date}
+                        </li>
+                    ))}
+                </ul>
+            </section>
         </Layout>
     );
 }
 
+export async function getStaticProps(): Promise<{ props: { allPostsData: any } }> {
+    const allPostsData = getSortedPostsData();
+    return {
+        props: {
+            allPostsData,
+        },
+    };
+}
